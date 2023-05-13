553d9859be7b4c8e707df20e6739d283547672e3
Robert <robertyishi@gmail.com>
2023-05-12 22:19:39 -0700

    solve 6-piece endgame

--- mpi-run.sh
+++ mpi-run.sh
@@ -5,10 +5,10 @@
 #SBATCH --nodes=24
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-#SBATCH --time=02:00:00
+#SBATCH --time=72:00:00
 
 cd bin
 module load gcc openmpi
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 # Usage: ./solve <n-pieces> <n-threads> <memory-in-GiB>
-mpirun ./solve 3 32 90
+mpirun ./solve 4 32 90
